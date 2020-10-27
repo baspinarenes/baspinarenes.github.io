6ca9612d956200b465cfe37416ebb0b9b51471a6
Enes Başpınar <enesbaspinar07@gmail.com>
2020-10-27 23:04:11 +0300

    jquerry eklendi

---
+++ assets/js/table.js
@@ -0,0 +1,21 @@
+function tableFindFunc() {
+
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("findWord");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("dictTable");
+  tr = table.getElementsByTagName("tr");
+
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
