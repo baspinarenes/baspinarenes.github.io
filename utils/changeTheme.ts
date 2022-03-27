d1d0b3220e7633b56228c645103df9ab6460f53c
Enes Başpınar <enesbaspinar07@gmail.com>
2022-03-27 10:03:54 +0300

    Fix theme changer code

--- utils/changeTheme.ts
+++ utils/changeTheme.ts
@@ -1,20 +1,12 @@
 const changeTheme = () => {
-  if (localStorage.getItem("color-theme")) {
-    if (localStorage.getItem("color-theme") === "light") {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
-    }
+  const currentColorTheme = localStorage.getItem("color-theme");
+
+  if (currentColorTheme && currentColorTheme === "light") {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("color-theme", "dark");
   } else {
-    if (document.documentElement.classList.contains("dark")) {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
-    }
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("color-theme", "light");
   }
 };
 
