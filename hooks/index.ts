5f5bd5ff270316c8cbe4ee08bb0673008b923601
Enes Başpınar <enesbaspinar07@gmail.com>
2022-04-02 01:51:17 +0300

    Add google analytics

---
+++ hooks/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+import useHandleRouteChange from "./useHandleRouteChange";
+
+export { useHandleRouteChange };
---
+++ utils/gtag.ts
@@ -0,0 +1,21 @@
+import { GTagEvent } from "../models/gtag";
+
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+const pageview = (url: string) => {
+  (window as any).gtag("config", GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+const event = ({ action, category, label, value }: GTagEvent) => {
+  (window as any).gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value,
+  });
+};
+
+export { GA_TRACKING_ID, pageview, event };
