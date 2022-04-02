cd0dfb3b7a7c348abd9ebd057cc160bb37eab98f
Enes Başpınar <enesbaspinar07@gmail.com>
2022-04-02 22:59:10 +0300

    Try new way for google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,29 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import Script from "next/script";
+import { useRouter } from "next/router";
 import PageLayout from "../components/templates/PageLayout";
 import { HamburgerMenuContextProvider } from "../contexts/hamburger-menu";
-import { useHandleRouteChange } from "../hooks";
 import * as gtag from "../utils/gtag";
 import "../styles/globals.scss";
 
 const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
+
   const [isOpenHamburgerMenu, setIsOpenHamburgerMenu] =
     useState<boolean>(false);
+  const router = useRouter();
 
-  useHandleRouteChange();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   const contextData = useMemo(
     () => ({ isOpenHamburgerMenu, setIsOpenHamburgerMenu }),
@@ -26,24 +36,6 @@ const MyApp = (props: AppProps) => {
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
         <Component {...pageProps} />
       </PageLayout>
     </HamburgerMenuContextProvider>
