e61c3d61da90f39d1bfd67c3da2c9f68fa0e5ada
Enes Başpınar <enesbaspinar07@gmail.com>
2022-04-10 01:48:22 +0300

    fix social card for twitter

--- components/elements/common/Meta.tsx
+++ components/elements/common/Meta.tsx
@@ -1,7 +1,6 @@
 // https://gist.github.com/reginpv/3f2918f4d605b9ec967b3ab80a040355
 
 import Head from "next/head";
-import SocialCardImage from "images/social-card.webp";
 import siteData from "../../../constants/site-data";
 
 const Meta = (props: MetaProps) => {
@@ -11,35 +10,41 @@ const Meta = (props: MetaProps) => {
     <Head>
       <title>{title ? `${title} | ${siteData.title}` : siteData.title}</title>
       <meta name="description" content={siteData.description} />
-
       {/* <!-- Google / Search Engine Tags --> */}
       <meta itemProp="name" content={title ?? siteData.author.name} />
       <meta
         itemProp="description"
         content={description ?? siteData.description}
       />
-      <meta itemProp="image" content={SocialCardImage.src} />
-
+      <meta
+        itemProp="image"
+        content={`${siteData.url}/images/social-card.png`}
+      />
       {/* <!-- Facebook Meta Tags --> */}
       <meta property="og:title" content={title ?? siteData.author.name} />
       <meta
         property="og:description"
         content={description ?? siteData.description}
       />
-      <meta property="og:image" content={SocialCardImage.src} />
+      <meta
+        property="og:image"
+        content={`${siteData.url}/images/social-card.png`}
+      />
       <meta
         property="og:url"
         content={url ? `${siteData.url}${url}` : siteData.url}
       />
       <meta property="og:type" content="website" />
-
       {/* <!-- Twitter Meta Tags --> */}
       <meta name="twitter:title" content={title ?? siteData.author.name} />
       <meta
         name="twitter:description"
         content={description ?? siteData.description}
       />
-      <meta name="twitter:image" content={SocialCardImage.src} />
+      <meta
+        name="twitter:image"
+        content={`${siteData.url}/images/social-card.png`}
+      />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
