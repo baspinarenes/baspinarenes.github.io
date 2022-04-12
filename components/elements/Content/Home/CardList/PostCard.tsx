7965bf7dcdebaab0bd893e2027d6ad2cd60a00b2
Enes Başpınar <enesbaspinar07@gmail.com>
2022-04-12 20:22:43 +0300

    add popular posts feature

--- components/elements/Content/Home/CardList/PostCard.tsx
+++ components/elements/Content/Home/CardList/PostCard.tsx
@@ -1,40 +1,40 @@
 import Image from "next/image";
 import { Icons } from "components/common";
 
-interface PostCardProps {
-  name: string;
-  viewerCount: number;
-  description: string;
-  type: string;
-  postUrl: string;
-}
-
 const PostCard = (props: PostCardProps) => {
-  const { name, viewerCount, description, type, postUrl } = props;
+  const { title, views, summary, type, slug } = props;
 
   return (
     <a
-      href={postUrl}
-      className="flex flex-col border border-gray-300 p-4 rounded-xl hover:bg-gray-100"
+      href={slug}
+      className="-mx-4 border-y -mb-[1px] rounded-none flex flex-col sm:border border-gray-300 dark:border-gray-600 p-4 sm:mx-0 sm:rounded-xl hover:bg-gray-100 hover:dark:bg-gray-800"
     >
-      <div className="flex justify-between">
-        <div className="font-bold text-yellow-500">
+      <div className="flex gap-1">
+        <div className="text-yellow-500 mr-auto">
           {type === "translate" ? "TRANSLATER" : "WRITER"}
         </div>
         <div className="flex">
-          <span>{viewerCount}</span>
           <Image
             src={Icons.User}
             alt="Post viewer icon"
             height={20}
             width={20}
           />
+          <span className="text-yellow-500 ml-[2px]">{views}</span>
         </div>
       </div>
-      <h3 className="font-bold">{name}</h3>
-      <p>{description}</p>
+      <h3 className="font-bold">{title}</h3>
+      <p className="dark:text-gray-400">{summary.slice(0, 75)}...</p>
     </a>
   );
 };
+
+interface PostCardProps {
+  title: string;
+  views: number;
+  summary: string;
+  type: string;
+  slug: string;
+}
 
 export default PostCard;
