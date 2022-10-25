eee6acd0da9f6b0a0076d99cefd708fbbf9caffa
Enes Başpınar <enesbaspinar07@gmail.com>
2022-10-25 08:11:19 +0300

    Add test-test-diye-nice-nice-2-modul-mocklama article

--- constants/site-data.ts
+++ constants/site-data.ts
@@ -9,7 +9,7 @@ const siteData = {
     title: "Associate Developer",
     company: "Trendyol",
     summary:
-      'Kendimden çok kısa bahsetmem gerekirse, araştırma yapmaktan ve bir şeyler öğrenmekten keyif duyan birisiyim. Öğrendiklerimi aktarmayı severim ve bunun tezahürü olarak bloguma nacizane yazılar yazmaya çalışırım. Temel mottom; tekdüze ve sıkıcı blog yazılarından insanları kurtarmak. Okuduğum çoğu yazıdan sonra çığlık atasım geliyor ve isyanımı buradan haykırıyorum. Yandaki emojinin sebebi de bu. Hatta ve hatta blogumun gayriresmi adı "Bir Yazılımcının Çığlıkları". Bu kadar goygoy yeter, devamı yazılarda!',
+      "Kendimden çok kısa bahsetmem gerekirse, araştırma yapmaktan ve bir şeyler öğrenmekten keyif duyan birisiyim. Öğrendiklerimi aktarmayı seviyorum ve bunun tezahürü olarak bloguma nacizane yazılar yazmaya çalışıyorum. Motivasyonum; tekdüze ve sıkıcı blog yazılarından insanları kurtarmak. Okuduğum çoğu yazıdan sonra çığlık atasım geliyor ve isyanımı buradan haykırıyorum. Yandaki emojinin sebebi de bu. Gelecek yazılarda buluşalım!",
   },
   description: "Abidik gubidik yazılım blogu.",
   footer: "2022 © No rights are reserved.",
